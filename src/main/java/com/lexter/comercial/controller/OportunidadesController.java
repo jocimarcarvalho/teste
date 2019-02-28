28473dd26a9c689c079c4183861a5b7d1d341501
jocimarcarvalho <jocimarccarvalho@hotmail.com>
2019-02-28 16:41:59 -0300

    segundo commit

--- src/main/java/com/lexter/comercial/controller/OportunidadesController.java
+++ src/main/java/com/lexter/comercial/controller/OportunidadesController.java
@@ -30,7 +30,7 @@ import com.lexter.comercial.repository.OportunidadeRepository;
 
 @RestController
 @RequestMapping("/oportunidades")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="localhost:4200")
 public class OportunidadesController {
 
 	@Autowired
